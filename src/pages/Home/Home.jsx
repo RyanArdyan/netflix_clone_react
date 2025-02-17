aef1e29726c683ce1bb445e4f5d1237086a1b9ab
RyanArdyan <ryanardyan437@gmail.com>
2025-02-17 23:13:05 +0700

    Menghias tampilan hero

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,9 @@ import React from 'react'
 import './Home.css';
 // import komponen Navbar untuk memanggil tampilan nya
 import Navbar from '../../components/Navbar/Navbar';
+// bahkan gambar harus di import
+import hero_banner from '../../assets/hero_banner.jpg';
+import hero_title from '../../assets/hero_title.png';
 
 // komponen adalah function yang mengembalikkan tampilan dan kode javascript
 const Home = () => {
@@ -11,6 +14,16 @@ const Home = () => {
     <div className="home">
       {/* memanggil komponen Navbar */}
       <Navbar/>
+      
+      <div className='hero'>
+        {/* import gambar yang sudah di import */}
+        <img src={hero_banner} alt='' className='banner-img' />
+        <div className='hero-caption'>
+        {/* import gambar yang sudah di import */}
+          <img src={hero_title} alt="" className='caption-img' />
+          <p>Discovering his ties to a secret ancient order, a young man living in modern istanbul embarks on a quest to save the city from an immortal enemy.</p>
+        </div>
+      </div>
     </div>
   )
 }
