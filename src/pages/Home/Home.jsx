a4dd8915ac172bb02c7d9578f9ecd2a4f25e7c6e
RyanArdyan <ryanardyan437@gmail.com>
2025-02-19 00:06:04 +0700

    update bos tanggal 19

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,6 +7,8 @@ import Navbar from '../../components/Navbar/Navbar';
 // bahkan gambar harus di import
 import hero_banner from '../../assets/hero_banner.jpg';
 import hero_title from '../../assets/hero_title.png';
+import play_icon from '../../assets/play_icon.png';
+import info_icon from '../../assets/info_icon.png';
 
 // komponen adalah function yang mengembalikkan tampilan dan kode javascript
 const Home = () => {
@@ -22,6 +24,17 @@ const Home = () => {
         {/* import gambar yang sudah di import */}
           <img src={hero_title} alt="" className='caption-img' />
           <p>Discovering his ties to a secret ancient order, a young man living in modern istanbul embarks on a quest to save the city from an immortal enemy.</p>
+          <div className='hero-btns'>
+            <button className='btn'>              
+              {/* panggil gambar yang sudah di import */}
+              <img src={play_icon} alt='' />
+               Play
+            </button>
+            <button className='btn dark-btn'>
+              <img src={info_icon} alt='' />
+              More Info
+            </button>
+          </div>
         </div>
       </div>
     </div>
